64001570597d94c4e0f89b4cee8e42686fb8c6c2
aditya bhojane <adityabhojane66@gmail.com>
2024-08-04 00:28:43 +0530

    add all fixes

--- src/components/Button/getButtonStyleType.js
+++ src/components/Button/getButtonStyleType.js
@@ -1,7 +1,7 @@
 function getButtonStyling(styleType) {
-    const primaryButtonStyling = "border border-[#7c7c00;] bg-gradient-to-r from-blue-400  hover:from-blue-600 text-[#000000] px-8";
+    const primaryButtonStyling = "border border-[#7c7c00;] bg-gradient-to-r from-blue-400 to-blue-800 hover:from-blue-800 hover:to-blue-400 text-[#fff] font-bold px-8";
     const secondaryButtonStyling = "bg-gray-500 border border-gray-700 hover:bg-gray-700 hover:border-gray-900";
-    const warningButtonStyling = "border border-[#7c7c00;] bg-gradient-to-r from-yellow-400  hover:from-yellow-500 text-[#000000] px-8";
+    const warningButtonStyling = "border border-[#7c7c00;] bg-gradient-to-r from-yellow-500 to-yellow-800 hover:from-yellow-800 hover:to-yellow-500 text-[#fff] font-bold px-8";
     const errorButtonStyling = "bg-red-500 border border-red-700 hover:bg-red-600 hover:border-red-900";
 
     if(styleType === "primary") {
